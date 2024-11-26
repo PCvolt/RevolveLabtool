d5d5ac7b434e31f84c5154f6ca0da917b94b4f5a
PC_volt <PC_volt@DESKTOP-I20R60L>
2024-11-26 17:56:54 +0100

    Use getDynamicAddress() for fetching addresses

--- src/revolveLib/Character.cpp
+++ src/revolveLib/Character.cpp
@@ -3,10 +3,10 @@
 
 namespace revolve
 {
-Character & character1 = *reinterpret_cast<Character *>(getBaseAddress() + Address::Character1);
-Character & character2 = *reinterpret_cast<Character *>(getBaseAddress() + Address::Character2);
+Character & character1 = *reinterpret_cast<Character *>(getDynamicAddress(Address::Character1));
+Character & character2 = *reinterpret_cast<Character *>(getDynamicAddress(Address::Character2));
 CharacterResources & characterResources1 =
-	*reinterpret_cast<CharacterResources *>(getBaseAddress() + Address::CharacterResources1);
+	*reinterpret_cast<CharacterResources *>(getDynamicAddress(Address::CharacterResources1));
 CharacterResources & characterResources2 =
-	*reinterpret_cast<CharacterResources *>(getBaseAddress() + Address::CharacterResources2);
+	*reinterpret_cast<CharacterResources *>(getDynamicAddress(Address::CharacterResources2));
 } // namespace revolve
