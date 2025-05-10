b461e18f52eebb993af1ad283da3e16b390fdad0
$PC_volt <$pcvoltinlab@gmail.com>
2025-05-10 18:24:37 +0200

    Added ButtonsPressed class

---
+++ src/labtool/ButtonsPressed.cpp
@@ -0,0 +1,40 @@
+#include "ButtonsPressed.hpp"
+
+namespace
+{
+//-----------------------------------------------------------------------------
+[[nodiscard]] int bitwiseOrIfPressed(int bits, bool isButtonPressed, int value)
+{
+	if (isButtonPressed)
+	{
+		bits |= value;
+	}
+
+	return bits;
+}
+} // namespace
+
+namespace labtool
+{
+//-----------------------------------------------------------------------------
+int ButtonsPressed::buildButtonsBitsVariable()
+{
+	int bits = 0;
+
+	bits = ::bitwiseOrIfPressed(bits, up, 0x1);
+	bits = ::bitwiseOrIfPressed(bits, down, 0x2);
+	bits = ::bitwiseOrIfPressed(bits, left, 0x4);
+	bits = ::bitwiseOrIfPressed(bits, right, 0x8);
+	bits = ::bitwiseOrIfPressed(bits, a, 0x10);
+	bits = ::bitwiseOrIfPressed(bits, b, 0x20);
+	bits = ::bitwiseOrIfPressed(bits, c, 0x40);
+	bits = ::bitwiseOrIfPressed(bits, d, 0x80);
+	bits = ::bitwiseOrIfPressed(bits, start, 0x100);
+	bits = ::bitwiseOrIfPressed(bits, ab, 0x30);
+	bits = ::bitwiseOrIfPressed(bits, bc, 0x60);
+	bits = ::bitwiseOrIfPressed(bits, abc, 0x70);
+	bits = ::bitwiseOrIfPressed(bits, screen, 0x400);
+
+	return bits;
+}
+} // namespace labtool
